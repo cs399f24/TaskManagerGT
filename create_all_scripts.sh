fdd6276f2607c2ba94cca2718cc2ce230afbe397
Giovanny Teran <terang@moravian.edu>
2024-11-21 23:00:48 -0500

    Update create_all_scripts.sh

--- create_all_scripts.sh
+++ create_all_scripts.sh
@@ -5,6 +5,7 @@ chmod +x ./automation/create_dynamodb_table.sh
 chmod +x ./automation/create_add_tasks_lambda.sh
 chmod +x ./automation/create_delete_task_lambda.sh
 chmod +x ./automation/create_view_lambda.sh
+chmod +x ./automation/create_task_manager_lambda.sh  
 
 # Execute each script
 echo "Running create_dynamodb_table.sh..."
@@ -40,6 +41,15 @@ if [ $? -eq 0 ]; then
     echo "View Lambda function creation ran successfully."
 else
     echo "Error running create_view_lambda.sh"
+    exit 1
+fi
+
+echo "Running create_task_manager_lambda.sh..."  # New line for the task manager Lambda function creation
+./automation/create_task_manager_lambda.sh
+if [ $? -eq 0 ]; then
+    echo "Task Manager Lambda function creation ran successfully."
+else
+    echo "Error running create_task_manager_lambda.sh"
     exit 1
 fi
 
