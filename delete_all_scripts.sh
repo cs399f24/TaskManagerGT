994c38b3d4abdf1e2e05003e8608075177d358f6
Giovanny Teran <terang@moravian.edu>
2024-12-10 09:15:40 -0500

    Update delete_all_scripts.sh

--- delete_all_scripts.sh
+++ delete_all_scripts.sh
@@ -12,7 +12,7 @@ fi
 
 # Delete Lambda functions
 echo "Deleting add tasks Lambda function..."
-aws lambda delete-function --function-name <ADD_TASKS_LAMBDA_NAME>
+aws lambda delete-function --function-name CreateTask
 if [ $? -eq 0 ]; then
     echo "Add tasks Lambda function deleted successfully."
 else
@@ -21,7 +21,7 @@ else
 fi
 
 echo "Deleting delete task Lambda function..."
-aws lambda delete-function --function-name <DELETE_TASK_LAMBDA_NAME>
+aws lambda delete-function --function-name DeleteTask
 if [ $? -eq 0 ]; then
     echo "Delete task Lambda function deleted successfully."
 else
@@ -30,20 +30,11 @@ else
 fi
 
 echo "Deleting view Lambda function..."
-aws lambda delete-function --function-name <VIEW_LAMBDA_NAME>
+aws lambda delete-function --function-name GetTasks
 if [ $? -eq 0 ]; then
     echo "View Lambda function deleted successfully."
 else
     echo "Error deleting view Lambda function."
-    exit 1
-fi
-
-echo "Deleting task manager Lambda function..."
-aws lambda delete-function --function-name <TASK_MANAGER_LAMBDA_NAME>
-if [ $? -eq 0 ]; then
-    echo "Task Manager Lambda function deleted successfully."
-else
-    echo "Error deleting task manager Lambda function."
     exit 1
 fi
 
